724a215f039e56bb587cff77e9eeefc57e23948c
zhengmaxwell <zhengmaxwell@gmail.com>
2020-03-17 18:25:02 -0400

    added controls

--- src/SerialCommunication.py
+++ src/SerialCommunication.py
@@ -2,3 +2,22 @@ import serial
 import time
 import RPi.GPIO as GPIO
 
+s = serial.Serial('/dev/ttyS3', 9600)
+
+while True:
+    input = s.read()
+
+    if input == -1:
+        s.write('S') # STOP
+    else:
+        s.write('G') # GO
+
+    if input == 1:
+        s.write('X') # X Motor
+    if input == 2:
+        s.write('Y') # Y Motor
+    if input == 3:
+        s.write('Z') # Z Motor
+
+    if input == 0:
+        s.write('F') # Flip Direction
